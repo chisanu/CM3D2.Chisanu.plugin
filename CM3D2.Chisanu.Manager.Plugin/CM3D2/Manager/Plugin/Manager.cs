5761209e7329dab57fc5fdf80c151f4105fa8679
Phạm Thái Bình <binh.email.office@gmail.com>
2016-06-22 21:47:57 +0700

    Manager quản lí các Plugin khác

---
+++ CM3D2.Chisanu.Manager.Plugin/CM3D2/Manager/Plugin/Manager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace CMD2.ChisanuManager.Plugin
+{
+    public delegate void VoidMethod();
+    public delegate void OneParamMethod<T>(T param);
+    public delegate void TwoParamMethod<A, B>(A paramA, B paramB);
+
+  
+ 
+    public class Manager
+    {
+        #region Quản lí các plugin khác
+
+        private static Manager _manager=null;
+        public static Manager manager
+        {
+            set { if (_manager == null) _manager = value;}
+            get { return _manager==null?manager=new Manager():_manager; }
+        }
+
+        public  List<PluginInfo> allPlugin = new List<PluginInfo>();
+
+        //Các thông tin trao đổi thêm giữa các plugin
+        public Dictionary<string, UnityEngine.Object> extraInfoManager = new Dictionary<string, UnityEngine.Object>();
+
+        public Manager()
+        {
+           // manager = new Manager();
+            allPlugin = new List<PluginInfo>();
+        }
+        #endregion
+
+
+
+    }
+
+    public class PluginInfo
+    {
+       public  string name;
+       public  string version;
+       public  string iconPath;
+       public  bool isActiveInScene;   //Cho phép kích hoạt trong scene
+       public  bool isShowing;         // true để mở giao diện UI
+
+        //Các thông tin mở rộng của plugin
+        public Dictionary<string, UnityEngine.Object> extraInfoPlugin = new Dictionary<string, UnityEngine.Object>();
+
+        public PluginInfo(string _name, string _version, string _iconPath=null, Dictionary<string, UnityEngine.Object> _extraData=null)
+        {
+            this.name = _name;
+            this.version = _version;
+            this.iconPath = _iconPath;
+            this.extraInfoPlugin = _extraData;
+        }
+        public PluginInfo() { }
+    }
+}
---
+++ CM3D2.Chisanu.Manager.Plugin/CM3D2/Manager/Plugin/ShareREsources.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Collections;
+namespace CMD2.ChisanuManager.Plugin
+{
+    /// <summary>
+    /// Có chức năng load dữ liệu, nhằm hạn chế những hàm nặng gây tốn thời gian
+    /// Các plugin khác sẽ yêu cầu dữ liệu gì bằng cách nạp properties cho hàm này trong start
+    /// hàm này sẽ đợi 5 fram để lấy hết các yêu cầu rồi chdck những yêu cầu cần gì
+    /// trả lại các yêu cầu với các delegate
+    /// </summary>
+    /// 
+    public enum OrderType
+    {
+        none,
+        UpdateListMaid
+
+    }
+    public class ManagerResources : MonoBehaviour
+    {
+        /// <summary>
+        /// Các order từ các plugin khác vd (bool)Order["MaidFirst"] = true; là yêu cầu Maid[0];
+        /// </summary>
+        public Dictionary<OrderType, UnityEngine.Object> Order = new Dictionary<OrderType, UnityEngine.Object>();
+        public List<Maid> existMaids = new List<Maid>();
+
+        public static ManagerResources manager { set; get; }
+        void Awake()
+        {
+            if (manager == null) manager = this;
+            if (manager != this) Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+
+
+        IEnumerator Start()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                yield return new WaitForEndOfFrame();
+            }
+
+            if(Order.ContainsKey( OrderType.UpdateListMaid))
+            {
+                 UpdateListMaid();
+            }
+            
+        }
+        public void UpdateListMaid()
+        {
+            existMaids = new List<Maid>();
+            for (int i = 0; i < GameMain.Instance.CharacterMgr.GetMaidCount(); i++)
+            {
+                Maid existMaid = GameMain.Instance.CharacterMgr.GetMaid(i);
+                if (existMaid != null)
+                {
+                    existMaids.Add(existMaid);
+                }
+            }
+        }
+
+    }
+}
